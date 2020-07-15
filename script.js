852277f7ca1020f38a68c8183a2c5ec40bf9a4b3
NaMN-0 <naman1474@gmail.com>
2020-07-15 12:03:32 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    $('.picContain1').animate({left:"60%"},500);
+    $('.picContain2').animate({left:"10%"},1000);
+    $('.picContain3').animate({left:"30%"},1500);
+
+});
